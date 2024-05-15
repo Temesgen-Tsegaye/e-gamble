7b825b0a572db6b1dd9da77ef294199db7cda11f
Temesgen Tsegaye <temesgentsegaye9891@gmail.com>
2024-05-15 15:53:42 +0300

    buggy

--- src/routes/component/tube.jsx
+++ src/routes/component/tube.jsx
@@ -4,9 +4,12 @@ import { div } from 'three/examples/jsm/nodes/Nodes.js';
 import Ball from './ball';
 import { forwardRef } from 'react';
 const Tube=forwardRef(function Tube(props,ref){
+
+              console.log(props,'pp')
+       let filterd
        if(props.next){
-           var filterd=props.generatedNumbers?.slice(10)
-        
+         filterd =props.generatedNumbers?.slice(10)
+        console.log(filterd)
 
        }else{
              filterd=props.generatedNumbers?.slice(0,10)
@@ -14,7 +17,7 @@ const Tube=forwardRef(function Tube(props,ref){
 return (
 <div  ref={ref} className='relative w-[7.5vh] h-[75vh]  bg-white rounded-lg'>
 
-{props.generatedNumbers?.map((val,index)=><Ball number={val} key={val}  classIndex={props.next?`ballright${val}`:`ball${val}`}/>)}
+{filterd?.map((val,index)=><Ball number={val} key={val}  classIndex={props.next?`ballright${val}`:`ball${val}`}/>)}
 </div>
 )
 }) 
